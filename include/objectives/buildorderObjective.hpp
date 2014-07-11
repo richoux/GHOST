8980a3b8eb8de215456dbdc9ff9b1558ee6a6c53
Florian Richoux <florian.richoux@polytechnique.edu>
2014-07-11 17:31:44 +0900

    Add buildorderObjective.hpp

---
+++ include/objectives/buildorderObjective.hpp
@@ -0,0 +1,105 @@
+/*
+ * GHOST (General meta-Heuristic Optimization Solving Tool) is a C++ library 
+ * designed for StarCraft: Brood war. 
+ * GHOST is a meta-heuristic solver aiming to solve any kind of combinatorial 
+ * and optimization RTS-related problems represented by a CSP/COP. 
+ * It is a generalization of the project Wall-in.
+ * Please visit https://github.com/richoux/GHOST for further information.
+ * 
+ * Copyright (C) 2014 Florian Richoux
+ *
+ * This file is part of GHOST.
+ * GHOST is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * GHOST is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with GHOST. If not, see http://www.gnu.org/licenses/.
+ */
+
+
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include "objective.hpp"
+#include "../variables/action.hpp"
+#include "../domains/buildorderDomain.hpp"
+
+using namespace std;
+
+namespace ghost
+{
+  /***********************/
+  /* BuildOrderObjective */
+  /***********************/
+  class BuildOrderObjective : public Objective<Action, BuildOrderDomain>
+  {
+  public:
+    BuildOrderObjective( const string & );
+
+  protected:
+    virtual void v_setHelper( const Action &b, const vector< Action > *vecVariables, const BuildOrderDomain *domain );
+
+    virtual double v_postprocessSatisfaction( vector< Action > *vecVariables,
+					      BuildOrderDomain *domain,
+					      double &bestCost,
+					      vector<int> &bestSolution ) const;
+    
+    virtual double v_postprocessOptimization( vector< Action > *vecActions,
+					      BuildOrderDomain *domain,
+					      double &bestCost );
+  };
+  
+  /***********/
+  /* NoneObj */
+  /***********/
+  class NoneObj : public BuildOrderObjective
+  {
+  public:
+    NoneObj();
+
+  private:
+    double v_cost( const vector< Action > *vecVariables, const BuildOrderDomain *domain ) const;
+    int v_heuristicVariable( const vector< int > &vecId, const vector< Action > *vecVariables, BuildOrderDomain *domain );
+    void v_setHelper( const Action &b, const vector< Action > *vecVariables, const BuildOrderDomain *domain );
+    double v_postprocessOptimization( vector< Action > *vecVariables, BuildOrderDomain *domain, double &bestCost );
+  };
+
+  /*******************/
+  /* MakeSpanMinCost */
+  /*******************/
+  class MakeSpanMinCost : public BuildOrderObjective
+  {
+  public:
+    MakeSpanMinCost();
+
+  private:
+    double v_cost( const vector< Action > *vecVariables, const BuildOrderDomain *domain ) const;
+    int v_heuristicVariable( const vector< int > &vecId, const vector< Action > *vecVariables, BuildOrderDomain *domain );
+    void v_setHelper( const Action &b, const vector< Action > *vecVariables, const BuildOrderDomain *domain );
+    double v_postprocessOptimization( vector< Action > *vecVariables, BuildOrderDomain *domain, double &bestCost );
+  };
+
+  /*******************/
+  /* MakeSpanMaxProd */
+  /*******************/
+  class MakeSpanMaxProd : public BuildOrderObjective
+  {
+  public:
+    MakeSpanMaxProd();
+
+  private:
+    double v_cost( const vector< Action > *vecVariables, const BuildOrderDomain *domain ) const;
+    int v_heuristicVariable( const vector< int > &vecId, const vector< Action > *vecVariables, BuildOrderDomain *domain );
+    void v_setHelper( const Action &b, const vector< Action > *vecVariables, const BuildOrderDomain *domain );
+    double v_postprocessOptimization( vector< Action > *vecVariables, BuildOrderDomain *domain, double &bestCost );
+  };
+}
