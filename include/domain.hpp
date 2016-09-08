967b44cd097786800960978f9f9364fdc832709b
Florian Richoux <florian.richoux@polytechnique.edu>
2016-09-08 13:54:11 +0200

    Remove virtual methods in Domain and update doc

--- include/domain.hpp
+++ include/domain.hpp
@@ -61,91 +61,7 @@ namespace ghost
     vector< int > initialDomain;	//!< Vector of integers containing the initial values of the domain.
     int outsideScope;			//!< Value representing all values outside the scope of the domain
     Random random;			//!< A random generator used by the function randomValue. 
-    
-    virtual bool v_isInitialized() const
-    {
-      return !currentDomain.empty();
-    }
-    
-    virtual void v_resetToInitial()
-    {
-      currentDomain.resize( initialDomain.size() );
-      std::copy( begin( initialDomain ), end( initialDomain ), begin( currentDomain ) );
-    }
-    
-    virtual bool v_removeValue( int value )
-    {
-      int index = indexOf( value );
-      if( index == -1 )
-	return false;
-      else
-      {
-	currentDomain.erase( begin( currentDomain ) + index );
-	return true;
-      }
-      
-      // auto& it = std::find( begin( currentDomain ), end( currentDomain ), value );
-      // if( it == end( currentDomain ) )
-      // 	return false;
-      // else
-      // {
-      // 	currentDomain.erase( it );
-      // 	return true;
-      // }
-    }
-    
-    virtual int v_randomValue()
-    {
-      return currentDomain[ random.getRandNum( currentDomain.size() ) ];
-    }
-    
-    virtual size_t v_getSize() const
-    {
-      return currentDomain.size();
-    }
-    
-    virtual size_t v_getInitialSize() const
-    {
-      return initialDomain.size();
-    }
-    
-    virtual int v_maxValue() const
-    {
-      return *std::max_element( begin( currentDomain ), end( currentDomain ) );
-    }
-    
-    virtual int v_minValue() const
-    {
-      return *std::min_element( begin( currentDomain ), end( currentDomain ) );
-    }
-    
-    virtual int v_maxInitialValue() const
-    {
-      return *std::max_element( begin( initialDomain ), end( initialDomain ) );
-    }
-    
-    virtual int v_minInitialValue() const
-    {
-      return *std::min_element( begin( initialDomain ), end( initialDomain ) );
-    }
-    
-    virtual int v_getValue( int index ) const
-    {
-      if( index >=0 && index < currentDomain.size() )
-	return currentDomain[ index ];
-      else
-	return outsideScope;
-    }
-    
-    virtual int v_indexOf( int value ) const
-    {
-      auto it = std::find( begin( currentDomain ), end( currentDomain ), value );
-      if( it == end( currentDomain ) )
-	return -1;
-      else
-	return it - begin( currentDomain );
-    }
-    
+        
   public:
     //! Domain constructor.
     /*!
@@ -184,94 +100,131 @@ namespace ghost
       std::iota( begin( initialDomain ), end( initialDomain ), startValue );
     }
 
-    //! Inline function following the NVI idiom, calling v_isInitialized.
+    //! Inline function to know if the domain has been initialized.
     /*!
      * Used to know if the Domain object is just an empty shell or a properly 
      * initialized domain. In some cases, it can be convenient to instanciate 
      * a domain object first and to fill it up with values latter.
      * \return True if and only if the domain has been initialized (i.e., the current domain is not empty).
      */
-    inline bool isInitialized() const { return v_isInitialized(); }
+    inline bool isInitialized() const
+    {
+      return !currentDomain.empty();
+    }
 
-    //! Inline function following the NVI idiom, calling v_resetToInitial.
+    //! Inline function to reset the current domain, ie to make it correspond to the initial domain.
     /*!
      * Resets the set of current values to the set of initial values. 
      * Allow the recover all values in the domain if we filtered some of them. 
      */
-    inline void resetToInitial() { v_resetToInitial(); }
-
-    //! Inline function following the NVI idiom, calling v_removeValue.
+    inline void resetToInitial()
+    {
+      currentDomain.resize( initialDomain.size() );
+      std::copy( begin( initialDomain ), end( initialDomain ), begin( currentDomain ) );
+    }
+    
+    //! Function to remove a value in the current domain.
     /*!
-     * Deletes a given value from the set of current domain values.
+     * Deletes a given value among the current domain values.
      * \param value is the value to remove from the domain
      * \return True if the value has been removed. False if the value to remove was not in the current domain.
      */
-    inline bool removeValue( int value ) { return v_removeValue( value ); }
+    bool removeValue( int value )
+    {
+      int index = indexOf( value );
+      if( index == -1 )
+	return false;
+      else
+      {
+	currentDomain.erase( begin( currentDomain ) + index );
+	return true;
+      }
+    }
 
-    //! Inline function following the NVI idiom, calling v_randomValue.
-    /*!
-     * Returns a random value from the domain.
-     */
-    inline int randomValue() { return v_randomValue(); }
+    //! Inline function returning a random value from the domain.
+    inline int randomValue()
+    {
+      return currentDomain[ random.getRandNum( currentDomain.size() ) ];
+    }
 
-    //! Inline function following the NVI idiom, calling v_getSize.
+    //! Inline function to get the size of the current domain.
     /*!
      * Get the number of values currently contained by the domain.
      * \return A size_t corresponding to the size of currentDomain.
      */
-    inline size_t getSize() const { return v_getSize(); }
+    inline size_t getSize() const
+    {
+      return currentDomain.size();
+    }
 
-    //! Inline function following the NVI idiom, calling v_getInitialSize.
+    //! Inline function to get the size of the initial domain.
     /*!
      * Get the number of values initially contained by the domain.
      * \return A size_t corresponding to the size of initialDomain.
      */    
-    inline size_t getInitialSize() const { return v_getInitialSize(); }
+    inline size_t getInitialSize() const
+    {
+      return initialDomain.size();
+    }
 
-    //! Inline function following the NVI idiom, calling v_maxValue.
-    /*!
-     * Get the highest value in the current domain. 
-     */
-    inline int maxValue() const { return v_maxValue(); }
+    //! Inline function to get the highest value in the current domain.
+    inline int maxValue() const
+    {
+      return *std::max_element( begin( currentDomain ), end( currentDomain ) );
+    }
 
-    //! Inline function following the NVI idiom, calling v_minValue.
-    /*!
-     * Get the lowest value in the current domain. 
-     */
-    inline int minValue() const { return v_minValue(); }
+    //! Inline function to get the lowest value in the current domain.
+    inline int minValue() const
+    {
+      return *std::min_element( begin( currentDomain ), end( currentDomain ) );
+    }
 
-    //! Inline function following the NVI idiom, calling v_maxInitialValue.
-    /*!
-     * Get the highest value in the initial domain. 
-     */
-    inline int maxInitialValue() const { return v_maxInitialValue(); }
+    //! Inline function to get the highest value in the initial domain.
+    inline int maxInitialValue() const
+    {
+      return *std::max_element( begin( initialDomain ), end( initialDomain ) );
+    }
 
-    //! Inline function following the NVI idiom, calling v_minInitialValue.
-    /*!
-     * Get the lowest value in the initial domain. 
-     */
-    inline int minInitialValue() const { return v_minInitialValue(); }
+    //! Inline function to get the lowest value in the initial domain.
+    inline int minInitialValue() const
+    {
+      return *std::min_element( begin( initialDomain ), end( initialDomain ) );
+    }
 
-    //! Inline function following the NVI idiom, calling v_getValue.
+    //! Get the value at the given index
     /*!
-     * Get the value at the given index
      * \param index is the index of the desired value.
      * \return The value at the given index if this one is in the range of the domain, otherwise the outside-the-scope value.
      */    
-    inline int getValue( int index ) const { return v_getValue( index ); }
+    int getValue( int index ) const
+    {
+      if( index >=0 && index < currentDomain.size() )
+	return currentDomain[ index ];
+      else
+	return outsideScope;
+    }
 
-    //! Inline function following the NVI idiom, calling v_indexOf.
+    //! Get the index of a given value.
     /*!
-     * Get the index of a given value.
      * \return If the given value is in the domain, it returns its index, and -1 otherwise.
      */ 
-    inline int indexOf( int value ) const { return v_indexOf( value ); }
+    int indexOf( int value ) const
+    {
+      auto it = std::find( begin( currentDomain ), end( currentDomain ), value );
+      if( it == end( currentDomain ) )
+	return -1;
+      else
+	return it - begin( currentDomain );
+    }
 
     //! Inline function returning the outside-scope value.
     /*!
      * Returns the value outsideScope.
      */ 
-    inline int getOutsideScope() const { return outsideScope; }
+    inline int getOutsideScope() const
+    {
+      return outsideScope;
+    }
     
     //! friend override of operator<<
     /*!
