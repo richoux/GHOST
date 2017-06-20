f9d2a2620bc18bce01f317333a78fd732593c0d1
Florian Richoux <florian.richoux@polytechnique.edu>
2017-06-20 16:02:25 +0200

    Start tests on Variable

--- test/src/test_variable.cpp
+++ test/src/test_variable.cpp
@@ -4,18 +4,27 @@
 class VariableTest : public ::testing::Test
 {
 public:
-  ghost::Variable *variable;
+  ghost::Variable *var_ctor1;
+  ghost::Variable *var_ctor2;
+  ghost::Variable *var_ctor2_bis;
+  ghost::Variable *var_ctor3;
 
   VariableTest()
   {
-    std::string name("Julie");
-    std::string shortname("J");
-    variable = new ghost::Variable( name, shortname );
+    // std::string name("Julie");
+    // std::string shortname("J");
+    var_ctor1 = new ghost::Variable( "Thomas", "T" );
+    var_ctor2 = new ghost::Variable( "Leo", "L", 0, std::vector(1,3,5,7,9), 0 );
+    var_ctor2_bis = new ghost::Variable( "Leo_b", "L_b", 0, std::vector(1,3,5,7,9) );
+    var_ctor3 = new ghost::Variable( "Julie", "J", 1, 3, 7 );
   }
 
   ~VariableTest()
   {
-    delete variable;
+    delete var_ctor1;
+    delete var_ctor2;
+    delete var_ctor2_bis;
+    delete var_ctor3;
   }
 };
 
@@ -23,7 +32,7 @@ TEST_F(VariableTest, isInitialized)
 {
   //EXPECT_STREQ( variable->getName(), "Julie" );
   //EXPECT_STREQ( variable->getShortName(), "j" );
-  EXPECT_FALSE( variable->hasInitializedDomain() );
+  EXPECT_FALSE( var_ctor1->hasInitializedDomain() );
 }
 
 
