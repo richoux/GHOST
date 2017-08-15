40f6f595bf04f1c2031ffc00bb9ff9a3f284abe0
Florian Richoux <florian.richoux@polytechnique.edu>
2017-08-15 04:31:56 +0200

    ManahaManaMahuf

---
+++ travis.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+git clone https://github.com/google/googletest.git
+
+# install GTest
+cd googletest/googletest
+mkdir build ; cd build
+cmake ..
+make
+cp -r ../include/* ../../../test/include/
+cp libg*a ../../../test/lib/
+
+# install GMock
+cd ../../googlemock
+mkdir build ; cd build
+cmake ..
+make
+cp -r ../include/* ../../../test/include/
+cp libg*a ../../../test/lib/
+cd ../../..  
+
+# install GHOST
+export LD_LIBRARY_PATH=/usr/local/lib/
+mkdir build
+cd build
+cmake ..
+make VERBOSE=1
+sudo make install
+cd ..
+
+# - make CXX=g++-5
+# - sudo make install
+
+# make and run tests
+make test
+cd test/bin && for i in $(ls); do ./$i; done
+
+# - valgrind --leak-check=full --show-reachable=yes bin/ghost --auto
+# - valgrind --tool=cachegrind bin/ghost --auto
