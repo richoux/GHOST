6db181107343de58b75b2aca2ed0e639e7867c1e
Florian Richoux <florian.richoux@polytechnique.edu>
2017-08-19 05:26:22 +0200

    Update doxygen and add some files

---
+++ search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "cdghimnoprsuv~",
+  1: "cdnorsv",
+  2: "g",
+  3: "cdmorsv",
+  4: "cdghinoprsuv~",
+  5: "dhinprsv",
+  6: "o",
+  7: "g"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables",
+  6: "related",
+  7: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Variables",
+  6: "Friends",
+  7: "Pages"
+};
+
