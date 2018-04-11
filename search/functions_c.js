f0c67d649db7029dfa2769e24886f82c3701e780
Florian Richoux <florian.richoux@polytechnique.edu>
2018-04-11 09:43:09 +0200

    Update Variable doc

--- search/functions_c.js
+++ search/functions_c.js
@@ -4,5 +4,5 @@ var searchData=
   ['_7edomain',['~Domain',['../classghost_1_1Domain.html#aa5d4791c2f90c021d9f19746f4694039',1,'ghost::Domain']]],
   ['_7eobjective',['~Objective',['../classghost_1_1Objective.html#aade47b263cbe7cae55e237b423dc4895',1,'ghost::Objective']]],
   ['_7erandom',['~Random',['../classghost_1_1Random.html#a5b2a324a97684d12174a6f97bdb43aaa',1,'ghost::Random']]],
-  ['_7evariable',['~Variable',['../classghost_1_1Variable.html#a718686ab389bd31ae0f3cbb312c9be77',1,'ghost::Variable']]]
+  ['_7evariable',['~Variable',['../classghost_1_1Variable.html#a06aa85e69ddf9a887dcc31c61dca70ab',1,'ghost::Variable']]]
 ];
