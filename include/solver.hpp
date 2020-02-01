f8979954865bceb0b045abf56d5c24ec7f1ea459
Florian Richoux <florian.richoux@polytechnique.edu>
2020-02-01 12:16:57 +0900

    Change compilation to non-experimental algo by default

--- include/solver.hpp
+++ include/solver.hpp
@@ -136,7 +136,7 @@ namespace ghost
 		//! To factorize code like if (best > current) then best=current and update configuration
 		void update_better_configuration( double& best, const double current, vector<int>& configuration );
 
-#if defined(ADAPTIVE_SEARCH)
+#if !defined(EXPERIMENTAL)
 		//! To compute the vector of variables which are principal culprits for not satisfying the problem
 		vector< Variable* > compute_worst_variables( bool freeVariables, const vector<double>& costVariables );
 #endif
