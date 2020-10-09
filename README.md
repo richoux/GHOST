b0bcb72459105314cb71f70d0a130e71d755a1f1
Florian Richoux <florian.richoux@polytechnique.edu>
2020-10-09 21:54:08 +0900

    GitHub actions

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 GHOST [![1.0.1](https://img.shields.io/badge/stable-1.0.1-brightgreen.svg)](https://github.com/richoux/GHOST/releases/tag/1.0.1) [![1.0.x](https://img.shields.io/badge/latest-1.0.x-f57f17.svg)](https://github.com/richoux/GHOST/tree/develop)
 =====
 
-[![Build_Status](https://travis-ci.org/richoux/GHOST.svg?branch=master)](https://travis-ci.org/richoux/GHOST)
+[![Actions Status](https://github.com/richoux/GHOST/workflows/Linux_Release_Test/badge.svg)](https://github.com/richoux/GHOST/actions)
+[![Actions Status](https://github.com/richoux/GHOST/workflows/Macos_Release_Test/badge.svg)](https://github.com/richoux/GHOST/actions)
+[![Actions Status](https://github.com/richoux/GHOST/workflows/Windows_Release_Test/badge.svg)](https://github.com/richoux/GHOST/actions)
 
 [![License](https://img.shields.io/badge/License-GNU_GPL_v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0.en.html)
 
-GHOST (General meta-Heuristic Optimization Solving Tool) is a C++ library designed to help developers to model and implement optimization problem solving. It contains a meta-heuristic solver aiming to solve any kind of combinatorial and optimization real-time problems represented by a Constraint Satisfaction Problem (CSP), a Constrained Optimization Problem (COP) or a Cost Function Network (CFN).
+GHOST (General meta-Heuristic Optimization Solving Tool) is a C++ library designed to help developers to model and implement optimization problem solving. It contains a meta-heuristic solver aiming to solve any kind of combinatorial and optimization real-time problems represented by a Constraint Satisfaction Problem (CSP), a Constrained Optimization Problem (COP), a Error Function Satisfaction Problem (EFSP), or a Error Function Optimization Problem (EFOP).
 
 ## License
 
--- build.sh
+++ build.sh
@@ -23,161 +23,161 @@ CYAN='\033[0;36m'
 NC='\033[0m' # No Color
 
 if [ "$OS" == "Darwin" ]; then
-  RELEASE="$RELEASE$OSX"
-  RELEASEBENCH="$RELEASEBENCH$OSX"
-  RELEASETRACE="$RELEASETRACE$OSX"
-  DEBUG="$DEBUG$OSX"
-  CXX="clang++"
+    RELEASE="$RELEASE$OSX"
+    RELEASEBENCH="$RELEASEBENCH$OSX"
+    RELEASETRACE="$RELEASETRACE$OSX"
+    DEBUG="$DEBUG$OSX"
+    CXX="clang++"
 fi
 
 function usage()
 {
-  echo "$0: usage: build.sh [release|bench|trace|debug|clean|doc|tests] [EXP]"
-  exit 1
+    echo "$0: usage: build.sh [release|bench|trace|debug|clean|doc|tests] [EXP]"
+    exit 1
 }
 
 function release()
 {
-  mkdir -p $RELEASE
-  cd $RELEASE
-  cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=$CXX $EXPERIMENTAL ..
-  make
-  sudo make install
+    mkdir -p $RELEASE
+    cd $RELEASE
+    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=$CXX $EXPERIMENTAL ..
+    make
+    sudo make install
 }
 
 function debug()
 {
-  mkdir -p $DEBUG
-  cd $DEBUG
-  cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=$CXX $EXPERIMENTAL ..
-  make
-  sudo make install
+    mkdir -p $DEBUG
+    cd $DEBUG
+    cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=$CXX $EXPERIMENTAL ..
+    make
+    sudo make install
 }
 
 function trace()
 {
-  mkdir -p $RELEASETRACE
-  cd $RELEASETRACE
-  cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DTRACE=ON -DCMAKE_CXX_COMPILER=$CXX $EXPERIMENTAL ..
-  make
-  sudo make install    
+    mkdir -p $RELEASETRACE
+    cd $RELEASETRACE
+    cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DTRACE=ON -DCMAKE_CXX_COMPILER=$CXX $EXPERIMENTAL ..
+    make
+    sudo make install    
 }
 
 function bench()
 {
-  mkdir -p $RELEASEBENCH
-  cd $RELEASEBENCH
-  cmake -DCMAKE_BUILD_TYPE=Release -DBENCH=ON -DCMAKE_CXX_COMPILER=$CXX $EXPERIMENTAL ..
-  make
-  sudo make install    
+    mkdir -p $RELEASEBENCH
+    cd $RELEASEBENCH
+    cmake -DCMAKE_BUILD_TYPE=Release -DBENCH=ON -DCMAKE_CXX_COMPILER=$CXX $EXPERIMENTAL ..
+    make
+    sudo make install    
 }
 
 function clean()
 {
-	if [ -d "$RELEASE" ]; then 
-		cd $RELEASE
-		make clean
-		sudo rm -fr *
-		cd ..
-	fi
-	if [ -d "$RELEASEBENCH" ]; then 
-		cd $RELEASEBENCH
-		make clean
-		sudo rm -fr *
-		cd ..
-	fi
-	if [ -d "$RELEASETRACE" ]; then 
-		cd $RELEASETRACE
-		make clean
-		sudo rm -fr *
-		cd ..
-	fi
-	if [ -d "$DEBUG" ]; then 
-		cd $DEBUG
-		make clean
-		sudo rm -fr *
-		cd ..
-	fi
-	if [ -d "build" ]; then 
-		cd build
-		make clean
-		sudo rm -fr *
-		cd ..
-	fi
+    if [ -d "$RELEASE" ]; then 
+	cd $RELEASE
+	make clean
+	sudo rm -fr *
+	cd ..
+    fi
+    if [ -d "$RELEASEBENCH" ]; then 
+	cd $RELEASEBENCH
+	make clean
+	sudo rm -fr *
+	cd ..
+    fi
+    if [ -d "$RELEASETRACE" ]; then 
+	cd $RELEASETRACE
+	make clean
+	sudo rm -fr *
+	cd ..
+    fi
+    if [ -d "$DEBUG" ]; then 
+	cd $DEBUG
+	make clean
+	sudo rm -fr *
+	cd ..
+    fi
+    if [ -d "build" ]; then 
+	cd build
+	make clean
+	sudo rm -fr *
+	cd ..
+    fi
 }
 
 function doc()
 {
-	doxygen doc/Doxyfile
+    doxygen doc/Doxyfile
 }
 
 function tests()
 {
-	cd tests
-	mkdir -p build
-	cd build
-	cmake ..
-	make
+    cd tests
+    mkdir -p build
+    cd build
+    cmake ..
+    make
 }
 
 function first_compile()
 {
-	if [ "$OS" == "Linux" ]; then
-		echo -e "\n\n${RED}>>> If you compile ${GREEN}GHOST${RED} for the ${CYAN}first time${RED}, you probably need to run the following command: ${ORANGE}sudo ldconfig${NC}"
-	fi
+    if [ "$OS" == "Linux" ]; then
+	echo -e "\n\n${RED}>>> If you compile ${GREEN}GHOST${RED} for the ${CYAN}first time${RED}, you probably need to run the following command: ${ORANGE}sudo ldconfig${NC}"
+    fi
 
-	if [ "$OS" == "Darwin" ]; then
-		echo -e "\n\n${RED}>>> If you compile ${GREEN}GHOST${RED} for the ${CYAN}first time${RED}, you probably need to run the following command: ${ORANGE}sudo update_dyld_shared_cache${NC}"
-	fi
+    if [ "$OS" == "Darwin" ]; then
+	echo -e "\n\n${RED}>>> If you compile ${GREEN}GHOST${RED} for the ${CYAN}first time${RED}, you probably need to run the following command: ${ORANGE}sudo update_dyld_shared_cache${NC}"
+    fi
 }
 
 if [ $# -gt 2 ]; then
-	usage
+    usage
 fi
 
 if [ $# -eq 0 ]; then
-	release
-	first_compile
-	cd $BACKPWD
-	exit 0
+    release
+    first_compile
+    cd $BACKPWD
+    exit 0
 fi
 
 if [ $# -eq 2 ]; then
-  if [ "$2" == "EXP" ]; then
-		EXPERIMENTAL="-DEXPERIMENTAL=ON"
-  fi
+    if [ "$2" == "EXP" ]; then
+	EXPERIMENTAL="-DEXPERIMENTAL=ON"
+    fi
 fi
 
 if [ "$1" == "release" ]; then
-  release
-  first_compile
-  cd $BACKPWD
-  exit 0
+    release
+    first_compile
+    cd $BACKPWD
+    exit 0
 elif [ "$1" == "bench" ]; then
-  bench
-  cd $BACKPWD
-  exit 0
+    bench
+    cd $BACKPWD
+    exit 0
 elif [ "$1" == "trace" ]; then
-  trace
-  cd $BACKPWD
-  exit 0
+    trace
+    cd $BACKPWD
+    exit 0
 elif [ "$1" == "debug" ]; then
-  debug
-  cd $BACKPWD
-  exit 0
+    debug
+    cd $BACKPWD
+    exit 0
 elif [ "$1" == "clean" ]; then
-  clean
-  cd $BACKPWD
-  exit 0
+    clean
+    cd $BACKPWD
+    exit 0
 elif [ "$1" == "doc" ]; then
-  doc
-  cd $BACKPWD
-  exit 0
+    doc
+    cd $BACKPWD
+    exit 0
 elif [ "$1" == "tests" ]; then
-  tests
-  cd $BACKPWD
-  exit 0
+    tests
+    cd $BACKPWD
+    exit 0
 else
-  usage
+    usage
 fi
 
