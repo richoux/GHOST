54b5169d4be01324e37cf4f0d979328a9f9b6858
Florian Richoux <florian.richoux@polytechnique.edu>
2021-04-30 10:34:51 +0900

    Add Model class

---
+++ include/model.hpp
@@ -0,0 +1,65 @@
+/*
+ * GHOST (General meta-Heuristic Optimization Solving Tool) is a C++ library
+ * designed to help developers to model and implement optimization problem
+ * solving. It contains a meta-heuristic solver aiming to solve any kind of
+ * combinatorial and optimization real-time problems represented by a CSP/COP/CFN.
+ *
+ * GHOST has been first developped to help making AI for the RTS game
+ * StarCraft: Brood war, but can be used for any kind of applications where
+ * solving combinatorial and optimization problems within some tenth of
+ * milliseconds is needed. It is a generalization of the Wall-in project.
+ * Please visit https://github.com/richoux/GHOST for further information.
+ *
+ * Copyright (C) 2014-2021 Florian Richoux
+ *
+ * This file is part of GHOST.
+ * GHOST is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * GHOST is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with GHOST. If not, see http://www.gnu.org/licenses/.
+ */
+
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include "variable.hpp"
+#include "constraint.hpp"
+#include "objective.hpp"
+
+class NullObjective;
+
+template<typename ObjectiveType = NullObjective, typename ... ConstraintType>
+class Model
+{
+protected:
+	std::vector<Variable> variables; 
+	std::vector<std::shared_ptr<Constraint>> constraints; 
+	ObjectiveType objective;
+
+public:
+	Model( const std::vector<Variable>& variables, 
+	       const std::vector<std::shared_ptr<Constraint>>&	constraints,
+	       ObjectiveType objective )
+		: variables( variables ),
+		  constraints( constraints ),
+		  objective( objective )
+	{ }
+
+	Model( const Model& other ) = default;
+	Model( Model&& other ) = default;	
+	
+	Model& operator=( const Model& other ) = delete;
+	Model& operator=( Model&& other ) = delete;	
+
+	virtual ~Model() = default;
+};
