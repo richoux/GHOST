8a39b7733a01b895688e3366ab34c46d44e98ccf
Florian Richoux <florian.richoux@polytechnique.edu>
2022-01-05 17:15:34 +0900

    Change Windows linking rules in CMak

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 @echo off
 set "InstallPath=C:/Users/Public/Documents/ghost"
 set /p "InstallPath=Enter install path or press ENTER for default [%InstallPath%]: "
-cmake -G "Visual Studio 16 2019" -A Win32 -B "build32"
+cmake -G "Visual Studio 16 2019" -A Win32 -B "build32" -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL
 cmake --build build32 --config Release
 cmake --install build32 --prefix %InstallPath%
-cmake -G "Visual Studio 16 2019" -A x64 -B "build64"
+cmake -G "Visual Studio 16 2019" -A x64 -B "build64" -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded
 cmake --build build64 --config Release
 cmake --install build64 --prefix %InstallPath%
