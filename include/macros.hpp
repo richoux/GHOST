38e8027667a59291705f62e47f21ead451290254
Florian Richoux <florian.richoux@polytechnique.edu>
2022-06-22 14:17:13 +0900

    Update copyright year

--- include/macros.hpp
+++ include/macros.hpp
@@ -10,7 +10,7 @@
  * within some milliseconds, making it very suitable for highly reactive or embedded systems.
  * Please visit https://github.com/richoux/GHOST for further information.
  *
- * Copyright (C) 2014-2021 Florian Richoux
+ * Copyright (C) 2014-2022 Florian Richoux
  *
  * This file is part of GHOST.
  * GHOST is free software: you can redistribute it and/or
