eb7f0ab9dd19c6b6e564c1628c9f48d118ea15a3
Florian Richoux <florian.richoux@polytechnique.edu>
2022-06-29 10:15:14 +0900

    Documentation from 2022-06-29

--- search/all_8.js
+++ search/all_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['linear_5fequation_2ehpp_39',['linear_equation.hpp',['../linear__equation_8hpp.html',1,'']]],
-  ['linearequation_40',['LinearEquation',['../classghost_1_1global__constraints_1_1LinearEquation.html',1,'ghost::global_constraints::LinearEquation'],['../classghost_1_1global__constraints_1_1LinearEquation.html#a6fd4955bc02de33c929fd564eec20e5f',1,'ghost::global_constraints::LinearEquation::LinearEquation()']]]
+  ['linearequation_40',['LinearEquation',['../classghost_1_1global__constraints_1_1LinearEquation.html',1,'ghost::global_constraints::LinearEquation'],['../classghost_1_1global__constraints_1_1LinearEquation.html#a6fd4955bc02de33c929fd564eec20e5f',1,'ghost::global_constraints::LinearEquation::LinearEquation(const std::vector&lt; int &gt; &amp;index, int rhs)'],['../classghost_1_1global__constraints_1_1LinearEquation.html#a024257fabca374bcf400e6135285f3fe',1,'ghost::global_constraints::LinearEquation::LinearEquation(const std::vector&lt; Variable &gt; &amp;variables, int rhs)']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['linearequation_138',['LinearEquation',['../classghost_1_1global__constraints_1_1LinearEquation.html#a6fd4955bc02de33c929fd564eec20e5f',1,'ghost::global_constraints::LinearEquation']]]
+  ['linearequation_138',['LinearEquation',['../classghost_1_1global__constraints_1_1LinearEquation.html#a6fd4955bc02de33c929fd564eec20e5f',1,'ghost::global_constraints::LinearEquation::LinearEquation(const std::vector&lt; int &gt; &amp;index, int rhs)'],['../classghost_1_1global__constraints_1_1LinearEquation.html#a024257fabca374bcf400e6135285f3fe',1,'ghost::global_constraints::LinearEquation::LinearEquation(const std::vector&lt; Variable &gt; &amp;variables, int rhs)']]]
 ];
